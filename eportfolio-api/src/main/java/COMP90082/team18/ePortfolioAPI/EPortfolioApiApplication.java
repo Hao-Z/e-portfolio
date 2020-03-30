80e5e1663029e30d0691f48b9424f5e71af53d7f
Hao-Z <15901414184@sina.cn>
2020-03-31 04:23:14 +1100

    Initial commit

---
+++ eportfolio-api/src/main/java/COMP90082/team18/ePortfolioAPI/EPortfolioApiApplication.java
@@ -0,0 +1,13 @@
+package COMP90082.team18.ePortfolioAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EPortfolioApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EPortfolioApiApplication.class, args);
+	}
+
+}
---
+++ eportfolio-api/src/test/java/COMP90082/team18/ePortfolioAPI/EPortfolioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package COMP90082.team18.ePortfolioAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EPortfolioApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
