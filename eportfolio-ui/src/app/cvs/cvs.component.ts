e504a1eb8571ecccd2b7cd94a7baeb89ddbc85b1
xsqc2 <49745741+xsqc2@users.noreply.github.com>
2020-04-06 21:59:31 +0800

    ok

--- eportfolio-ui/src/app/cvs/cvs.component.ts
+++ eportfolio-ui/src/app/cvs/cvs.component.ts
@@ -8,8 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class CVsComponent implements OnInit {
 
   constructor() { }
-  isCollapsed = false;
-  CVs = ["Name 1","Name 2","Name 3","Name 4","Name 5","Name 6","Name 7"];
+  isCollapsed = true;
+  CVs = ["Name 1","Name 2","Name 3","Name 4","Name 5","Name 6","Name 7","Name 8","Name 9","Name 10"];
   ngOnInit(): void {
   }
 
