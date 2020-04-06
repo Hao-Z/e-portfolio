3ae5306990af1173c6e36ca1ad810096b5428025
xsqc2 <49745741+xsqc2@users.noreply.github.com>
2020-04-06 18:41:08 +0800

    ok

--- eportfolio-ui/src/app/cvs/cvs.component.ts
+++ eportfolio-ui/src/app/cvs/cvs.component.ts
@@ -9,6 +9,7 @@ export class CVsComponent implements OnInit {
 
   constructor() { }
   isCollapsed = false;
+  CVs = ["Name 1","Name 2","Name 3","Name 4","Name 5","Name 6","Name 7"];
   ngOnInit(): void {
   }
 
