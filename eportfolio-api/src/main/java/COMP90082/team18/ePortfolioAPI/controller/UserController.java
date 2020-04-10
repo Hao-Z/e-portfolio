60d8f48621a9fb213f7cee8d6efa441bf121c95c
Hao-Z <15901414184@sina.cn>
2020-04-10 23:36:45 +1000

    Better jwt structure. Signup will also create jwt.

--- eportfolio-api/src/main/java/COMP90082/team18/ePortfolioAPI/controller/UserController.java
+++ eportfolio-api/src/main/java/COMP90082/team18/ePortfolioAPI/controller/UserController.java
@@ -2,12 +2,16 @@ package COMP90082.team18.ePortfolioAPI.controller;
 
 import COMP90082.team18.ePortfolioAPI.entity.Result;
 import COMP90082.team18.ePortfolioAPI.entity.User;
-import COMP90082.team18.ePortfolioAPI.repository.UserRepository;
+import COMP90082.team18.ePortfolioAPI.security.JWTMethod;
 import COMP90082.team18.ePortfolioAPI.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+
+import static COMP90082.team18.ePortfolioAPI.security.SecurityConstants.*;
 
 @CrossOrigin
 @RestController
@@ -16,12 +20,10 @@ public class UserController {
 
     @Autowired
     private UserService userService;
-    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @PostMapping(value = "/signup")
-    public Result signUp(@RequestBody User user){
-        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
-        return userService.signUp(user);
+    public Result signUp(@RequestBody User user, HttpServletResponse response){
+        return userService.signUp(user, response);
     }
 
 }
