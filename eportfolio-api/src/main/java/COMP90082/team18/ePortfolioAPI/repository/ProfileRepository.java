953c3daf3f32185b5a279c7c8e86c62fc0b7346b
Hao-Z <15901414184@sina.cn>
2020-04-15 02:18:36 +1000

    Profile repository and test.

--- eportfolio-api/src/main/java/COMP90082/team18/ePortfolioAPI/repository/ProfileRepository.java
+++ eportfolio-api/src/main/java/COMP90082/team18/ePortfolioAPI/repository/ProfileRepository.java
@@ -1,8 +1,9 @@
 package COMP90082.team18.ePortfolioAPI.repository;
 
 import COMP90082.team18.ePortfolioAPI.entity.Profile;
+import COMP90082.team18.ePortfolioAPI.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProfileRepository extends JpaRepository<Profile, Long> {
-
+    Profile findByUser(User user);
 }
