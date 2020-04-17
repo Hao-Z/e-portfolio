182d66508d696ccae7fc923cfc676cca68319700
xsqc2 <49745741+xsqc2@users.noreply.github.com>
2020-04-17 16:52:15 +0800

    sider collapse - ok

--- eportfolio-ui/src/app/layout/layout.component.ts
+++ eportfolio-ui/src/app/layout/layout.component.ts
@@ -7,11 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LayoutComponent implements OnInit {
 
-  isCollapsed : boolean;
+  isCollapsed = window.innerWidth < Number(770);
   constructor() { }
 
   ngOnInit(): void {
-    this.isCollapsed = window.innerWidth < Number(770);
+
   }
 
 }
