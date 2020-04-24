19c5793a961d4cfcd1c3781107d8a6a5dc605ebb
Hao-Z <15901414184@sina.cn>
2020-04-25 01:14:17 +1000

    Introduction and about entity, service, and comtroller.

--- eportfolio-api/src/main/java/COMP90082/team18/ePortfolioAPI/controller/AboutController.java
+++ eportfolio-api/src/main/java/COMP90082/team18/ePortfolioAPI/controller/AboutController.java
@@ -1,5 +1,7 @@
 package COMP90082.team18.ePortfolioAPI.controller;
 
+import COMP90082.team18.ePortfolioAPI.DTO.AboutDTO;
+import COMP90082.team18.ePortfolioAPI.DTO.IntroductionDTO;
 import COMP90082.team18.ePortfolioAPI.DTO.ProfileDTO;
 import COMP90082.team18.ePortfolioAPI.DTO.Result;
 import COMP90082.team18.ePortfolioAPI.service.ProfileService;
@@ -9,19 +11,18 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("users/{id}/profile")
-public class ProfileController {
+@RequestMapping("users/{id}/about")
+public class AboutController {
     @Autowired
     private ProfileService profileService;
 
     @GetMapping
-    public Result<ProfileDTO> getProfile(@PathVariable Long id){
-        return profileService.getProfile(id);
+    public Result<AboutDTO> getAbout(@PathVariable Long id){
+        return profileService.getAbout(id);
     }
 
     @PatchMapping
-    public Result<ProfileDTO> patchProfile(@PathVariable Long id, @RequestBody ProfileDTO profileDTO){
-        return profileService.patchProfile(id, profileDTO.toProfile());
+    public Result<AboutDTO> patchAbout(@PathVariable Long id, @RequestBody AboutDTO aboutDTO){
+        return profileService.patchAbout(id, aboutDTO.toProfile());
     }
-
 }
---
+++ eportfolio-api/src/main/java/COMP90082/team18/ePortfolioAPI/entity/Gender.java
@@ -0,0 +1,11 @@
+package COMP90082.team18.ePortfolioAPI.entity;
+
+public enum Gender {
+    FEMALE(0),
+    MALE(1),
+    OTHER(2);
+
+    Gender(int code) {
+    }
+
+}
