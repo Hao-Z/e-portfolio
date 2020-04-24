ab5361233d5892a8018b75355d2fa0d410afd15a
Hao Zhang <45502053+Hao-Z@users.noreply.github.com>
2020-04-24 21:56:35 +1000

    Revert "Welcome"

--- eportfolio-ui/src/app/home/home.component.ts
+++ eportfolio-ui/src/app/home/home.component.ts
@@ -1,57 +1,8 @@
 import { Component } from '@angular/core';
 
-// @Component({
-//   selector: 'app-home',
-//   template: `
-//     <app-top-bar></app-top-bar>
-//     <section class="hero is-info is-bold">
-//     <div class="hero-body">
-//     <div class="container">
-
-//       <h1 class="title">Home Page!</h1>
-
-//     </div>
-//     </div>
-//     </section>
-//   `,
-//   styles: [`
-//     .hero {
-//       background-image: url('/assets/homepage.jpg') !important;
-//       background-size: cover;
-//       height:500pt;
-//       background-position: center center;
-//     }
-//   `]
-// })
 @Component({
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  templateUrl: './home.component.html'
 })
-
 export class HomeComponent {
-  config;
-  fullpage_api;
-
-  constructor() {
-    // this is just an example => for more details on config please visit fullPage.js docs
-    this.config = {
-      sectionsColor: ['#FF543C', '#098DE9', '#FC6171', '#FEBC04'],
-      navigation: false,
-      slidesNavigation: true,
-      slidesNavPosition: 'bottom',
-      scrollOverflow: true,
-      paddingTop: '3em',
-      cards: true,
-      controlArrows: true,
-   
-    };
-  }
-
-  ngOnInit() {
-  }
-
-  getRef(fullPageRef) {
-    this.fullpage_api = fullPageRef;
-  }
 
 }
