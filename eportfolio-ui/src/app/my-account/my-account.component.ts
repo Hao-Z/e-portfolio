8a49569908ae4a3a1d71ccdd719bc454cc18d86d
xsqc2 <49745741+xsqc2@users.noreply.github.com>
2020-04-26 01:03:25 +0800

    UPDATE my account

--- eportfolio-ui/src/app/my-account/my-account.component.ts
+++ eportfolio-ui/src/app/my-account/my-account.component.ts
@@ -6,6 +6,7 @@ import * as globals from "../../global";
 import {jwt} from "../../global";
 import {username} from "../../global";
 import {userID} from "../../global";
+import {refreshJwt} from "../../global";
 
 @Component({
   selector: 'app-my-account',
@@ -17,32 +18,36 @@ import {userID} from "../../global";
 export class MyAccountComponent implements OnInit {
 
   updateForm: FormGroup;
-  profiles_value = new Map<string,any>();
+  profiles_value : any;
   editable = new Map<string,boolean>();
-  controlsConfig : {[key:string]:any} = {};
   profiles = ['username', 'email', 'birthday', 'phoneNumber'];
 
   constructor(private http: HttpClient, private route: ActivatedRoute, private formBuilder: FormBuilder) {
 
-    //TODO: Specify each input field.
-    for(let p of this.profiles){
-      this.controlsConfig[p] = [''];
-    }
-    this.updateForm = this.formBuilder.group(this.controlsConfig);
+    this.updateForm = new FormGroup({
+      'username': new FormControl(''),
+      'email': new FormControl('', [Validators.email]),
+      'birthday': new FormControl(''),
+      'phoneNumber': new FormControl(''),
+    });
   }
 
   ngOnInit(): void {
-
+    refreshJwt();
     // this.getProfile();
-    for(let p of this.profiles){
-      this.profiles_value.set(p,p+"_value");
-    }
+    this.profiles_value = {
+      'username': 'un',
+      'email': 'e',
+      'birthday': 'bd',
+      'phoneNumber': 'pn',
+    };
     for(let p of this.profiles){
       this.editable.set(p,false);
     }
   }
 
   getProfile(){
+    refreshJwt();
     this.http.get<any>(globals.backend_path + userID + "/profile",{
       observe: 'response',
     }).subscribe((result:any)=>{
@@ -52,20 +57,22 @@ export class MyAccountComponent implements OnInit {
 
   data:any;
   onSubmit(key, value) {
+    refreshJwt();
     //TODO:use PATCH to update profiles partially.
     if(key == 'email'){
-      this.data = {"user":{key:value}};
+      this.data = {"user":{[key]:value}};
     }else{
-      this.data = {key:value};
+      this.data = {[key]:value};
     }
 
     this.http.patch<any>(globals.backend_path + userID + "/profile", this.data, {
       observe: 'response',
     }).subscribe((result) => {
       // This code will be executed when the HTTP call returns successfully
+      this.profiles_value[key] = value;
       alert(result.headers.get("Authorization"))
     });
-
+    //this.profiles_value[key] = value;
     alert('Changes succeed: ' + JSON.stringify(this.data));
   }
 
