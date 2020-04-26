255f642e9e49641f53d8e0d5c44f5cd65f7492f9
xsqc2 <49745741+xsqc2@users.noreply.github.com>
2020-04-27 00:18:19 +0800

    update explore page

--- eportfolio-ui/src/app/explore/explore.component.ts
+++ eportfolio-ui/src/app/explore/explore.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {NzFormatEmitEvent, NzTreeNodeOptions} from "ng-zorro-antd";
 
 @Component({
   selector: 'app-cvs',
@@ -22,7 +23,32 @@ export class ExploreComponent implements OnInit {
   constructor() { }
   isCollapsed = false;
   CVs = ["Name 1","Name 2","Name 3","Name 4","Name 5","Name 6","Name 7","Name 8","Name 9","Name 10"];
+  sortValues: any;
+  defalutValue: any;
+  nodes: NzTreeNodeOptions[];
+  defaultCheckedKeys: any;
+  defaultSelectedKeys: any;
+
   ngOnInit(): void {
+    this.nodes = [
+      {title: 'demo1', key: 'demo1', isLeaf: true},
+      {title: 'demo2', key: 'demo2', isLeaf: true},
+      {title: 'demo3', key: 'demo3', isLeaf: true},
+      {title: 'demo4', key: 'demo4', isLeaf: true},
+    ];
+    this.defalutValue = null;
+    this.sortValues = ['demo1','demo2','demo3'];
   }
 
+  getSearch(){
+    alert('searched')
+  }
+
+  nzClick($event: NzFormatEmitEvent) {
+
+  }
+
+  nzCheck($event: NzFormatEmitEvent) {
+
+  }
 }
