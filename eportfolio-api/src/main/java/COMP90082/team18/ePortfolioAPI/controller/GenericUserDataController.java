1203d3ff7cb62b2e4a093c77cb64d17bc3777380
Hao-Z <15901414184@sina.cn>
2020-05-02 16:29:48 +1000

    Create empty profile after sign up. Small bug fixed.

--- eportfolio-api/src/main/java/COMP90082/team18/ePortfolioAPI/controller/GenericUserDataController.java
+++ eportfolio-api/src/main/java/COMP90082/team18/ePortfolioAPI/controller/GenericUserDataController.java
@@ -9,7 +9,6 @@ import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import javax.websocket.server.PathParam;
 import java.lang.reflect.Type;
 import java.util.Arrays;
 import java.util.HashMap;
