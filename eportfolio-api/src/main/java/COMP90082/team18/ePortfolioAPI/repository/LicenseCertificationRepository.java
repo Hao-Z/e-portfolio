bf1bf6be9102bfd83ab0dd1d78f8cfd934b3b874
Hao-Z <15901414184@sina.cn>
2020-05-02 22:15:34 +1000

    Beautify.

--- eportfolio-api/src/main/java/COMP90082/team18/ePortfolioAPI/repository/LicenseCertificationRepository.java
+++ eportfolio-api/src/main/java/COMP90082/team18/ePortfolioAPI/repository/LicenseCertificationRepository.java
@@ -1,10 +1,7 @@
 package COMP90082.team18.ePortfolioAPI.repository;
 
 import COMP90082.team18.ePortfolioAPI.entity.LicenseCertification;
-import COMP90082.team18.ePortfolioAPI.entity.User;
-
-import java.util.List;
 
 public interface LicenseCertificationRepository extends UserDataRepository<LicenseCertification> {
-    List<LicenseCertification> findByUser(User user);
+
 }
--- eportfolio-api/src/main/java/COMP90082/team18/ePortfolioAPI/repository/VolunteerExperienceRepository.java
+++ eportfolio-api/src/main/java/COMP90082/team18/ePortfolioAPI/repository/VolunteerExperienceRepository.java
@@ -1,10 +1,7 @@
 package COMP90082.team18.ePortfolioAPI.repository;
 
-import COMP90082.team18.ePortfolioAPI.entity.User;
 import COMP90082.team18.ePortfolioAPI.entity.VolunteerExperience;
 
-import java.util.List;
-
 public interface VolunteerExperienceRepository extends UserDataRepository<VolunteerExperience> {
-    List<VolunteerExperience> findByUser(User user);
+
 }
