fd28718bbeadc72db91168f782ccd00937111f55
Razr-9 <runze_n@icloud.com>
2020-05-08 04:41:14 +1000

    styles,animation and filter in explore page

--- eportfolio-ui/src/app/explore/explore.component.ts
+++ eportfolio-ui/src/app/explore/explore.component.ts
@@ -7,22 +7,23 @@ import {NzFormatEmitEvent, NzTreeNodeOptions} from "ng-zorro-antd";
   styleUrls: ['./explore.component.css']
 })
 export class ExploreComponent implements OnInit {
+  searchValue = '';
   isCollapsed = window.innerWidth < Number(770);
   userDatas = {
     firstName: 'Chuqiao',
     lastName: 'Chen',
     headline: 'Student of the University of Melbourne',
     education: 'the University of Melbourne',
-    industry: 'Information technology',
-    region: 'Melbourn, VIC',
+    industry: 'Information Technology',
+    region: 'Melbourne, VIC',
     email: 'chuqiao.chen@gmail.com',
-    phone: '(61)0400000000',
-    profileUrl: 'www.xxxx.xxxxxxxxxxxx.xxx'
+    phone: '(+61)0400000000',
+    profileUrl: 'www.xxxxxxxx.com'
   };
 
   constructor() { }
   CVs = [
-    {'industry': 'Information technology'},
+    {'industry': 'Information Technology'},
     {'industry': 'Computer Software'},
     {'industry': 'Computer Games'},
     {'industry': 'Computer Hardware'},
@@ -41,27 +42,36 @@ export class ExploreComponent implements OnInit {
   };**/
 
 industryColor: any = {
-  "Information technology":"background-color : rgba(106, 141, 195, 0.7)",
+  "Information Technology":"background-color : rgba(106, 141, 195, 0.7)",
   "Computer Software":"background-color : rgba(124, 133, 138, 0.7)",
   "Computer Games":"background-color : rgba(15, 76, 129, 0.7)",
   "Computer Hardware":"background-color : rgba(244, 184, 148, 0.7)",
   "Computer Networking":"background-color : rgba(163, 141, 128, 0.7)",
 };
+test_nodes = [
+    {title: 'Information Technology', key: 'demo1', isLeaf: true},
+    {title: 'Computer Software', key: 'demo2', isLeaf: true},
+    {title: 'Computer Games', key: 'demo3', isLeaf: true},
+    {title: 'Computer Hardware', key: 'demo4', isLeaf: true},
+    {title: 'Computer Networking', key: 'demo5', isLeaf: true},
+]
   ngOnInit(): void {
-    this.nodes = [
-      {title: 'demo1', key: 'demo1', isLeaf: true},
-      {title: 'demo2', key: 'demo2', isLeaf: true},
-      {title: 'demo3', key: 'demo3', isLeaf: true},
-      {title: 'demo4', key: 'demo4', isLeaf: true},
-    ];
+    this.nodes = this.test_nodes;
     this.defalutValue = null;
-    this.sortValues = ['demo1','demo2','demo3'];
+    this.sortValues = ['order by xxx','order by xxx','order by xxx'];
+    
   }
 
   getSearch(){
     alert('searched')
   }
-
+  nzEvent(event: NzFormatEmitEvent): void {
+    // console.log(event);
+  }
+  filterSearch (node: NzTreeNodeOptions): boolean {
+    console.log(node)
+    return false
+  }
   nzCheck($event: NzFormatEmitEvent) {
 
   }
