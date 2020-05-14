11c10f004896a26d616a5204ec0e21a8bf474291
xsqc2 <49745741+xsqc2@users.noreply.github.com>
2020-05-14 22:23:18 +0800

    little update

--- eportfolio-ui/src/app/layout/layout.component.ts
+++ eportfolio-ui/src/app/layout/layout.component.ts
@@ -7,27 +7,12 @@ import {Router} from "@angular/router";
   styleUrls: ['./layout.component.css']
 })
 export class LayoutComponent implements OnInit {
-  width;
-
   isCollapsed = window.innerWidth < Number(770);
   constructor( private router: Router) { }
 
   ngOnInit(): void {
-    if(window.innerWidth < Number(770)){
-      this.width = "padding-left:100px";
-    }else{
-      this.width = "padding-left:256px";
-    }
     if(localStorage.getItem('jwt_token') == null){
       this.router.navigate(['/login'])
-    }
-  }
-
-  changeWidth() {
-    if(window.innerWidth < Number(770)){
-      this.width = "padding-left:100px";
-    }else{
-      this.width = "padding-left:256px";
     }
   }
 
