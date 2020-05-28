73dfaa4da2cda963e5d5ab0c6b8f8130f655727a
chenchuqiao <chuqiao.chan@gmail.com>
2020-05-29 06:51:05 +1000

    Add double check with delete. Fix bugs. Show access deny with cv-show.

--- eportfolio-ui/src/app/core/services/http-error-handler.service.ts
+++ eportfolio-ui/src/app/core/services/http-error-handler.service.ts
@@ -11,22 +11,22 @@ export class HttpErrorHandler {
     constructor(private alertService: AlertService) { }
 
     errorHandler(error: HttpErrorResponse) {
+        var err = { 'code': null, message: "" };
         if (error.error instanceof ErrorEvent) {
           // A client-side or network error occurred. Handle it accordingly.
           console.error('An error occurred:', error.error.message);
-          // this.alertService.error(`${error.error.message} Something bad happened, please try again later.`)
+          err.code = 0;
+          err.message = error.error.message
         } else {
           // The backend returned an unsuccessful response code.
           // The response body may contain clues as to what went wrong,
           console.log(
             `Backend returned code ${error.status}, ` +
-            `body was: ${JSON.stringify(error.error)}`);
-            // this.alertService.error(`Backend returned code ${error.status}, something bad happened, please try again later.`)        
+            `body was: ${JSON.stringify(error.error)}`);    
+            err.code = error.status;
+            err.message = JSON.stringify(error.error)  
           }
-          // this.alertService.error(`Something bad happened, please try again later.`)
-        // return an observable with a user-facing error message
-        return throwError(
-         'Something bad happened; please try again later.');
+        return throwError(err);
       }
 
 }
