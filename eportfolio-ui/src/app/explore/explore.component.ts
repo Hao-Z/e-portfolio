0b055c99df0327eaf135d889d5479e9d82837c13
xsqc2 <49745741+xsqc2@users.noreply.github.com>
2020-05-29 21:23:42 +0800

    Update explore.component.ts

--- eportfolio-ui/src/app/explore/explore.component.ts
+++ eportfolio-ui/src/app/explore/explore.component.ts
@@ -4,6 +4,7 @@ import * as globals from "../../global";
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 import { RouterLink } from '@angular/router';
 import { UniqueApiService } from 'src/app/core/services/unique-api.service';
+import {DataService} from "../core/services/data.service";
 
 @Component({
   selector: 'app-cvs',
@@ -31,7 +32,8 @@ export class ExploreComponent implements OnInit {
 
   constructor(
     private http: HttpClient,
-    private apiService: UniqueApiService
+    private apiService: UniqueApiService,
+    private dataService: DataService
   ) { }
   sortValues: any;
   gender_nodes: NzTreeNodeOptions[] = [
@@ -54,127 +56,10 @@ export class ExploreComponent implements OnInit {
 
     // <
     // this.getNodes();
-    this.nodes_str = [
-      { label: 'Accounting', value : '0' },
-      { label: 'Airlines/Aviation', value:'1' },
-      { label: 'Animation', value : '2' },
-      { label: 'Apparel & Fashion', value : '3' },
-      { label: 'Architecture & Planning', value : '4' },
-      { label: 'Arts & Crafts', value : '5' },
-      { label: 'Banking', value : '6' },
-      { label: 'Biotechnology', value : '7' },
-      { label: 'Broadcast Media', value : '8' },
-      { label: 'Building Materials', value : '9' },
-      { label: 'Business Supplies & Equipment', value : '10' },
-      { label: 'Capital Markets', value : '11' },
-      { label: 'Chemicals', value : '12' },
-      { label: 'Civic & Social Organization', value : '13' },
-      { label: 'Commercial Real Estate', value : '14' },
-      { label: 'Computer & Network Security', value : '15' },
-      { label: 'Computer Games', value : '16' },
-      { label: 'Computer Hardware', value : '17' },
-      { label: 'Computer Networking', value : '18' },
-      { label: 'Computer Software', value : '19' },
-      { label: 'Construction', value : '20' },
-      { label: 'Consumer Electronics', value : '21' },
-      { label: 'Consumer Goods', value : '22' },
-      { label: 'Consumer Services', value : '23' },
-      { label: 'Cosmetics', value : '24' },
-      { label: 'Dairy', value : '25' },
-      { label: 'Design', value : '26' },
-      { label: 'E-learning', value : '27' },
-      { label: 'Education Management', value : '28' },
-      { label: 'Electrical &amp; Electronic Manufacturing', value : '29' },
-      { label: 'Entertainment', value : '30' },
-      { label: 'Environmental Services', value : '31' },
-      { label: 'Events Services', value : '32' },
-      { label: 'Executive Office', value : '33' },
-      { label: 'Facilities Services', value : '34' },
-      { label: 'Farming', value : '35' },
-      { label: 'Financial Services', value : '36' },
-      { label: 'Fine Art', value : '37' },
-      { label: 'Fishery', value : '38' },
-      { label: 'Food &amp; Beverages', value : '39' },
-      { label: 'Food Production', value : '40' },
-      { label: 'Fundraising', value : '41' },
-      { label: 'Furniture', value : '42' },
-      { label: 'Gambling & Casinos', value : '43' },
-      { label: 'Glass, Ceramics & Concrete', value : '44' },
-      { label: 'Government Administration', value : '45' },
-      { label: 'Government Relations', value : '46' },
-      { label: 'Health, Wellness & Fitness', value : '47' },
-      { label: 'Services', value : '48' },
-      { label: 'Information Technology', value : '49' },
-      { label: 'Information Technology & Services', value : '50' },
-      { label: 'Insurance', value : '51' },
-      { label: 'International Trade & Development', value : '52' },
-      { label: 'Internet', value : '53' },
-      { label: 'Mechanical or Industrial Engineering', value : '54' },
-      { label: 'Media Production', value : '55' },
-      { label: 'Medical Device', value : '56' },
-      { label: 'Medical Practice', value : '57' },
-      { label: 'Mental Health Care', value : '58' },
-      { label: 'Military', value : '59' },
-      { label: 'Mining & Metals', value : '60' },
-      { label: 'Motion Pictures & Film', value : '61' },
-      { label: 'Museums & Institutions', value : '62' },
-      { label: 'Music', value : '63' },
-      { label: 'Nanotechnology', value : '64' },
-      { label: 'Newspapers', value : '65' },
-      { label: 'Non-profit Organization Management', value : '66' },
-      { label: 'Oil & Energy', value : '67' },
-      { label: 'Online Media', value : '68' },
-      { label: 'Outsourcing/Offshoring', value : '69' },
-      { label: 'Package/Freight Delivery', value : '70' },
-      { label: 'Packaging & Containers', value : '71' },
-      { label: 'Paper & Forest Products', value : '72' },
-      { label: 'Performing Arts', value : '73' },
-      { label: 'Pharmaceuticals', value : '74' },
-      { label: 'Philanthropy', value : '75' },
-      { label: 'Photography', value : '76' },
-      { label: 'Plastics', value : '77' },
-      { label: 'Political Organization', value : '78' },
-      { label: 'Primary/Secondary Education', value : '79' },
-      { label: 'Printing', value : '80' },
-      { label: 'Professional Training & Coaching', value : '81' },
-      { label: 'Program Development', value : '82' },
-      { label: 'Public Policy', value : '83' },
-      { label: 'Public Relations & Communications', value : '84' },
-      { label: 'Public Safety', value : '85' },
-      { label: 'Publishing', value : '86' },
-      { label: 'Railroad Manufacture', value : '87' },
-      { label: 'Ranching', value : '88' },
-      { label: 'Real Estate', value : '89' },
-      { label: 'Recreational Facilities & Services', value : '90' },
-      { label: 'Religious Institutions', value : '91' },
-      { label: 'Renewables & Environment', value : '92' },
-      { label: 'Research', value : '93' },
-      { label: 'Restaurants', value : '94' },
-      { label: 'Retail', value : '95' },
-      { label: 'Security & Investigations', value : '96' },
-      { label: 'Semiconductors', value : '97' },
-      { label: 'Shipbuilding', value : '98' },
-      { label: 'Sporting Goods', value : '99' },
-      { label: 'Sports', value : '100' },
-      { label: 'Staffing & Recruiting', value : '101' },
-      { label: 'Supermarkets', value : '102' },
-      { label: 'Telecommunications', value : '103' },
-      { label: 'Textiles', value : '104' },
-      { label: 'Think Tanks', value : '105' },
-      { label: 'Tobacco', value : '106' },
-      { label: 'Translation & Localization', value : '107' },
-      { label: 'Transportation/Trucking/Railroad', value : '108' },
-      { label: 'Utilities', value : '109' },
-      { label: 'Venture Capital & Private Equity', value : '110' },
-      { label: 'Veterinary', value : '111' },
-      { label: 'Warehousing', value : '112' },
-      { label: 'Wholesale', value : '113' },
-      { label: 'Wine & Spirits', value : '114' },
-      { label: 'Writing & Editing', value : '115'}
-    ];
+    this.nodes_str = this.dataService.getIndustryList();
     this.nodes = [];
     for(let n of this.nodes_str){
-      this.nodes.push({title: n['label'], key: n['label'], isLeaf: true, checked: false})
+      this.nodes.push({title: n['label'], key: n['value'], isLeaf: true, checked: false})
     }
     // > Delete
 
